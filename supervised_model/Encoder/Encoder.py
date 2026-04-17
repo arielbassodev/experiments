8248d4a9a1a8194a0dcf42ddfdbf060a3e5e1252
Ariel Basso Madjoukeng <ariel.bassomadjoukeng@unamur.be>
2026-04-17 16:37:46 +0200

    with positionnal encoding

---
+++ supervised_model/Encoder/Encoder.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=5000):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
+
+if __name__ == '__main__':
+    x = torch.randn(10,512)
+    pe = PositionalEncoding(512, max_len=5000)
+    pe.forward(x)
+
+#class LinearProjection(nn.Module):
+
+#class InputEmbedder(nn.Module):
+
+#class TransformerEncoder(nn.Module):
+
+#class VitTransformerEncoder(nn.Module):
