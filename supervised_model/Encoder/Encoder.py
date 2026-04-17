52f77efc20f96909458e0bb0d0eccf7aea8e4786
Ariel Basso Madjoukeng <ariel.bassomadjoukeng@unamur.be>
2026-04-17 22:39:45 +0200

    evolution_encoder

--- supervised_model/Encoder/Encoder.py
+++ supervised_model/Encoder/Encoder.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import math
-
+import einops
+from einops import rearrange, repeat, reduce
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
@@ -17,15 +18,46 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         return x + self.pe[:, :x.size(1)]
 
+class LinearProjection(nn.Module):
+    def __init__(self,input_dim, latent_size):
+        super().__init__()
+        self.latent_size = latent_size
+        self.input_dim = input_dim
+        self.linear_projection = nn.Linear(self.input_dim, self.latent_size)
+        self.class_token = nn.Parameter(torch.zeros(1, 1, self.latent_size), requires_grad=True)
+        self.positional_encoding = PositionalEncoding(self.latent_size)
+
+    def forward(self, sign_batch):
+        sign_batch = self.linear_projection(sign_batch)
+        class_token = self.class_token.expand(sign_batch.shape[0], -1, -1)
+        sign_batch = torch.cat((sign_batch, class_token), dim=1)
+        sign_batch = self.positional_encoding.forward(sign_batch)
+        return sign_batch
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, d_model, n_heads):
+        super().__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.layer = nn.TransformerEncoderLayer(d_model=self.d_model, nhead=self.n_heads,)
+        self.linear_projection = LinearProjection(self.d_model, self.d_model)
+        self.transformer = nn.TransformerEncoder(self.layer, self.n_heads)
+
+    def forward(self, x):
+        x = self.linear_projection(x)
+        mask = [True] * 20
+        mask_tensor = torch.tensor(mask)
+        x = self.transformer.forward(x, src_key_padding_mask=mask_tensor)
+        y = x
+        return x
+
 if __name__ == '__main__':
-    x = torch.randn(10,512)
-    pe = PositionalEncoding(512, max_len=5000)
-    pe.forward(x)
+    x = torch.randn(20, 50,512)
+    mask = [True] * 20
+    transformer = TransformerEncoder(512, 8)
+    x = transformer.forward(x)
+    print(x.shape)
 
-#class LinearProjection(nn.Module):
-
-#class InputEmbedder(nn.Module):
-
-#class TransformerEncoder(nn.Module):
 
 #class VitTransformerEncoder(nn.Module):
